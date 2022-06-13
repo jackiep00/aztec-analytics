ef52ba027efdbc7558232650e243d0e782afbe87
Jack Yin <jackyin0@gmail.com>
2022-06-13 11:54:53 -0500

    bridge leaderboard

--- dashboards/aztec-connect-kpis/bridge-leaderboard-by-asset.sql
+++ dashboards/aztec-connect-kpis/bridge-leaderboard-by-asset.sql
@@ -1,16 +1,24 @@
--- https://dune.com/queries/906060
+-- https://dune.com/queries/906311
 
 select bridge_protocol
     , l.description
+    , symbol as token_symbol
     , sum(abs_volume_usd) as all_time_volume_usd
     , sum(case when date between now() and now() - interval '7 days' then abs_volume_usd else 0 end) as "7_day_volume"
     , sum(case when date = now() then abs_volume_usd else 0 end) as last_day_volume
+    , sum(input_volume_usd) as all_time_input_volume_usd
+    , sum(case when date between now() and now() - interval '7 days' then input_volume_usd else 0 end) as "7_day_input_volume"
+    , sum(case when date = now() then input_volume_usd else 0 end) as last_day_input_volume
+    , sum(output_volume_usd) as all_time_output_volume_usd
+    , sum(case when date between now() and now() - interval '7 days' then output_volume_usd else 0 end) as "7_day_output_volume"
+    , sum(case when date = now() then output_volume_usd else 0 end) as last_day_output_volume
     , sum(num_rollups) as all_time_rollups
     , sum(case when date between now() and now() - interval '7 days' then num_rollups else 0 end) as "7_day_rollups"
     , sum(case when date = now() then num_rollups else 0 end) as last_day_rollups
+    , token_address as token_address
     , l.contract_creator as bridge_deployer
     , bridge_address    
 from dune_user_generated.view_aztec_v2_daily_bridge_activity b
 inner join dune_user_generated.aztec_v2_contract_labels l on b.bridge_address = l.contract_address
-group by 1,2,l.contract_creator,bridge_address
-order by 3 desc
+group by 1,2,3, token_address, l.contract_creator,bridge_address
+order by 4 desc
--- dashboards/aztec-connect-kpis/weekly-activity.sql
+++ dashboards/aztec-connect-kpis/weekly-activity.sql
@@ -3,8 +3,8 @@
 select date_trunc('week', date) as week 
     , count(distinct bridge_address) as bridge_contracts_active
     , sum(abs_volume_usd) as volume_usd
-    , sum(num_txns) as num_txns
-    , sum(abs_volume_usd) / sum(num_txns) as avg_txn_size_usd
+    , sum(num_rollups) as num_rollups -- a "txn" on L1 is actually an entire rollup of Aztec txns
+    , sum(abs_volume_usd) / sum(num_rollups) as avg_rollup_size_usd
 from dune_user_generated.view_aztec_v2_daily_bridge_activity
 group by 1
 order by 1 desc
