347b5c2c8f214f51ffad14398bc3169fac2d47b1
Jack Yin <jackyin0@gmail.com>
2022-06-13 09:31:00 -0500

    update contract labels and descriptions

--- views/contract_labels.sql
+++ views/contract_labels.sql
@@ -1,12 +1,13 @@
 -- https://dune.com/queries/874721
 -- prototype alias table for Aztec V2
-drop if exists table dune_user_generated.aztec_v2_contract_labels;
+drop table if exists dune_user_generated.aztec_v2_contract_labels cascade;
 
 create table if not exists dune_user_generated.aztec_v2_contract_labels
 (
   protocol varchar,
   contract_type varchar,               
-  version varchar,            
+  version varchar,
+  description text,
   contract_address bytea                       
 );
 
@@ -16,11 +17,12 @@ create index on dune_user_generated.aztec_v2_contract_labels(protocol);
 truncate table dune_user_generated.aztec_v2_contract_labels;
 
 insert into dune_user_generated.aztec_v2_contract_labels 
-(protocol,               contract_type,      version,            contract_address) values
-('Element',              'Bridge',           '0.1',              '\xb5e0Ab45C2c48a6F7032Ee0db749c3c9C5c58A32'::bytea),
-('Aztec RollupProcessor','Rollup',           '0.1',              '\xff6bed1e4d28491b89a02dc56b34a4b273eb9e0d'::bytea),
-('Lido',                 'Bridge',           '0.1',              '\xFDb2f2E720436972644bf824dEBea47F07C5041D'::bytea),
-('Aztec RollupProcessor','Rollup',           '1.0',              '\xFF1F2B4ADb9dF6FC8eAFecDcbF96A2B351680455'::bytea),
-('Element',              'Bridge',           '1.0',              '\xaeD181779A8AAbD8Ce996949853FEA442C2CDB47'::bytea),
-('Lido',                 'Bridge',           '1.0',              '\x381abF150B53cc699f0dBBBEF3C5c0D1fA4B3Efd'::bytea)
+(protocol,               contract_type,      version,   description,                   contract_address) values
+('Element',              'Bridge',           '0.1',     'Test - Beta bridge',           '\xb5e0Ab45C2c48a6F7032Ee0db749c3c9C5c58A32'::bytea), -- v 0.1 is pre-deployment beta
+('Aztec RollupProcessor','Rollup',           '0.1',     'Test - Beta rollup',           '\xff6bed1e4d28491b89a02dc56b34a4b273eb9e0d'::bytea),
+('Lido',                 'Bridge',           '0.1',     'Test - Beta bridge',           '\xFDb2f2E720436972644bf824dEBea47F07C5041D'::bytea),
+('Aztec RollupProcessor','Rollup',           '1.0',     'Prod Aztec Rollup',            '\xFF1F2B4ADb9dF6FC8eAFecDcbF96A2B351680455'::bytea),
+('Element',              'Bridge',           '1.0',     'Prod Element Bridge',          '\xaeD181779A8AAbD8Ce996949853FEA442C2CDB47'::bytea),
+('Lido',                 'Bridge',           '1.0',     'Prod Lido Bridge',             '\x381abF150B53cc699f0dBBBEF3C5c0D1fA4B3Efd'::bytea),
+('AceOfZK',              'Bridge',           '1.0',     'Ace Of ZK NFT - nonfunctional','\x0eb7f9464060289fe4fddfde2258f518c6347a70'::bytea)
 ;
