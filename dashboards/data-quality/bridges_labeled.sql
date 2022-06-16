f6f50236e137cc1c9ef167a0b8f9dd6f0b364ac3
Jack Yin <jackyin0@gmail.com>
2022-06-16 10:23:50 -0500

    add prelaunch bridge option

--- dashboards/data-quality/bridges_labeled.sql
+++ dashboards/data-quality/bridges_labeled.sql
@@ -1,5 +1,20 @@
 -- https://dune.com/queries/892818
 
+-- Prelaunch Bridges
+/*
+select "bridgeAddress"
+    , (CONCAT('<a href="https://etherscan.io/address/0'
+        , SUBSTRING("bridgeAddress"::text, 2, 42), '">0'
+        , SUBSTRING("bridgeAddress"::text, 2, 5),'...'
+        ,SUBSTRING("bridgeAddress"::text, 39, 42), '</a>')) as link
+    , coalesce(l.protocol, 'UNLABELED') as protocol_label
+    , l.version as label_version
+    , l.description
+from aztec_v2."RollupProcessorPreLaunch_evt_BridgeAdded" b
+left join dune_user_generated.aztec_v2_contract_labels l on b."bridgeAddress" = l.contract_address
+union
+*/
+-- Production bridges
 select "bridgeAddress"
     , (CONCAT('<a href="https://etherscan.io/address/0'
         , SUBSTRING("bridgeAddress"::text, 2, 42), '">0'
