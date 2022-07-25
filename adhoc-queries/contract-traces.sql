e32f217a69d20b035f05a6db6194ddf2a8bc685d
Jack Yin <jackyin0@gmail.com>
2022-07-25 13:21:47 -0400

    better documentation

--- adhoc-queries/contract-traces.sql
+++ adhoc-queries/contract-traces.sql
@@ -5,9 +5,10 @@ select table_type, table_schema
     , split_part(table_name,'_',2) as trace_type
     -- , split_part(table_name,'_',3) as trace_name_1 -- this doesn't account for additional underscores
     , right(table_name,
-        length(table_name) - 2 -- subtract the 2 underscores we're excluding
-        - length(split_part(table_name,'_',1)) -- contract_name
-        - length(split_part(table_name,'_',2)) -- trace_type
+        length(table_name) 
+        - 2 -- subtract the 2 underscores we're excluding
+        - length(split_part(table_name,'_',1)) -- subtract the contract_name
+        - length(split_part(table_name,'_',2)) -- subtract the trace_type
     ) as trace_name -- grab the rest of the trace name by excluding the strings we've already parsed
     , table_name
 from information_schema.tables
